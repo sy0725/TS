d3bc9a16f418fe088a368835897df194acc43d35
Jeon SunYong <sunyong0725@naver.com>
2023-11-03 14:26:48 +0900

    1103 수업 내용

--- dist/ch03/ex03-13.js
+++ dist/ch03/ex03-13.js
@@ -1,4 +1,22 @@
 "use strict";
 // ex03-13.ts 복사
 (() => {
+    // {} 빈 객체로 타입이 추론된 상태
+    var todo1 = {};
+    todo1.title = "할일 1";
+    // { title : string } 로 타입이 추론됨
+    var todo2 = {
+        title: "할일 2",
+    };
+    todo2.content = "내용2";
+    var todo3 = {
+        title: "할일 3 ",
+        content: "내용 3",
+    };
+    todo3.content = "수정 3";
+    const todo4 = {
+        title: "할일 4 ",
+        content: "내용 4",
+    };
+    console.log(todo1, todo2, todo3, todo4);
 })();
